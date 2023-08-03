dcb28e8e7cf8cf56a0bc157a859f7a8184c7ce45
Fabian <fabian@BigMacdeFabian.home>
2023-08-03 22:24:53 +0200

    pruebas rutas y consola

---
+++ resources/views/mostrar-fecha.blade.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<h1>$titulo</h1>";
+echo date('d-m-Y');
+echo "<h1>Fecha en Ingles</h1>";
+echo date('l jS \of F Y h:i:s A');
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h1>Hello World</h1>";
 });
+
+
+/*
+GET: CONSEGUIR DATOS
+POST: GUARDAR DATOS
+PUT: ACTUALIZAR RECURSOS
+DELETE: ELIMINAR RECURSOS
+
+*/
+
+Route::get('/mostrar-fecha', function () {
+    $titulo = "Estoy mostrando la fecha";
+    return view('mostrar-fecha', array(
+        'titulo' => $titulo
+    ));
+});
+
+
+//Route pelicula
+Route::get('/pelicula/{titulo?}/{year?}', function ($titulo = 'No hay una pelicula seleccionada', $year = 2019) {
+    return view('pelicula', array(
+        'titulo' => $titulo,
+        'year' => $year
+    ));
+})  ->where(array(
+    'titulo' => '[a-zA-Z]+',
+    'year' => '[0-9]+'
+));
